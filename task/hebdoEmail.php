c583269a08e61adcba019ca620f581c79a4957a4
Jb Lm <lemetayer.jb@gmail.com>
2024-02-04 17:40:29 +0100

    taches utiles pour les crons

---
+++ task/hebdoEmail.php
@@ -0,0 +1,67 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$f3 = \Base::instance();
+
+$f3->set('ROOT', __DIR__.'/..');
+$f3->set('UI', $f3->get('ROOT')."/views/");
+$f3->set('DB', new \DB\SQL('sqlite:'. $f3->get('ROOT').'/database/datas.sqlite'));
+$f3->set('ENCODING','UTF-8');
+$f3->set('LANGUAGE','fr-FR');
+$f3->config($f3->get('ROOT').'/config/app.conf');
+$f3->set('DEBUG', 3);
+
+$dateNextWeek = strtotime('+1 week', strtotime(date('Y-m-d')));
+$firstDayWeek = new \DateTime();
+$lastDayWeek = new \DateTime();
+$firstDayWeek->setTimestamp(strtotime('last monday', $dateNextWeek));
+$lastDayWeek->setTimestamp(strtotime('next friday', $dateNextWeek));
+
+$rows = $f3->get('DB')->exec('select b.start_date, b.user_id, b.title from booking b WHERE b.user_id IS NOT NULL AND b.start_date >= "'.$firstDayWeek->format('Y-m-d').'" AND b.start_date <= "'.$lastDayWeek->format('Y-m-d').'"');
+
+$resultPerDates = [];
+foreach ($rows as $row) {
+  if (!isset($resultPerDates[$row['start_date']])) {
+    $resultPerDates[$row['start_date']] = [];
+  }
+  $resultPerDates[$row['start_date']][] = $row['user_id'].' - '.$row['title'];
+}
+
+$nbJour = 0;
+$creneaux = [];
+
+while ($firstDayWeek <= $lastDayWeek) {
+    if ($nbJour != 2) {
+      if ($nbJour == 0 && !isset($resultPerDates[$firstDayWeek->format('Y-m-d')])) {
+        $creneaux[] = "2 personnes pour le ".$firstDayWeek->format('Y-m-d');
+      }
+      elseif ($nbJour == 0 && count($resultPerDates[$firstDayWeek->format('Y-m-d')]) == 1) {
+        $creneaux[] = "1 personne pour le ".$firstDayWeek->format('Y-m-d');
+      }
+      elseif (!isset($resultPerDates[$firstDayWeek->format('Y-m-d')])) {
+        $creneaux[] = "1 personne pour le ".$firstDayWeek->format('Y-m-d');
+      }
+    }
+    $nbJour++;
+    $firstDayWeek->modify('+1 day');
+}
+if ($creneaux) {
+  $rows = $f3->get('DB')->exec('select u.login, u.firstname, u.lastname, u.email from user u WHERE u.is_active = 1');
+  $counter = 0;
+  $success = 0;
+  foreach ($rows as $row) {
+    if (!$row['email'])
+      continue;
+    $f3->set('user', $row);
+    $f3->set('creneaux', $creneaux);
+    $message = \Template::instance()->render('emails/hebdo.html');
+    if (mail($row['email'], $f3->get('email.subject_hebdo'), $message, ['From' => $f3->get('email.from')])) {
+      $success++;
+    }
+    $counter++;
+  }
+  echo "$counter emails to send ($success sent successfully)";
+} else {
+  echo "0 email to send";
+}
---
+++ task/reminderEmail.php
@@ -0,0 +1,32 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$f3 = \Base::instance();
+
+$f3->set('ROOT', __DIR__.'/..');
+$f3->set('UI', $f3->get('ROOT')."/views/");
+$f3->set('DB', new \DB\SQL('sqlite:'. $f3->get('ROOT').'/database/datas.sqlite'));
+$f3->set('ENCODING','UTF-8');
+$f3->set('LANGUAGE','fr-FR');
+$f3->config($f3->get('ROOT').'/config/app.conf');
+$f3->set('DEBUG', 3);
+
+
+$date = date('Y-m-d', strtotime('+'.$f3->get('day_reminder_email_delay').' days'));
+$rows = $f3->get('DB')->exec('select u.login, u.firstname, u.lastname, u.email from user u INNER JOIN booking b ON u.id = b.user_id WHERE u.is_active = 1 AND b.start_date = "'.$date.'"');
+$counter = 0;
+$success = 0;
+foreach ($rows as $row) {
+  if (!$row['email'])
+    continue;
+  $f3->set('user', $row);
+  $f3->set('date', date('d/m/Y', strtotime('+'.$f3->get('day_reminder_email_delay').' days')));
+  $message = \Template::instance()->render('emails/reminder.html');
+  if (mail($row['email'], $f3->get('email.subject_reminder'), $message, ['From' => $f3->get('email.from')])) {
+    $success++;
+  }
+  $counter++;
+}
+
+echo "$counter emails to send ($success sent successfully)";
